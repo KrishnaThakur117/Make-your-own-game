67d8660f8b278a316cd12fb4d77f2f642ac0ed19
Krishna Thakur <71776030+KrishnaThakur117@users.noreply.github.com>
2020-11-04 17:21:37 +0400

    Add files via upload

--- box.js
+++ box.js
@@ -1,24 +1,23 @@
-class box{
+class Box{
     constructor(x,y,width,height){
      var options = {
-         'restitution':0.8,
+        /* 'restitution':0.8,
          'friction':1.0,
-         'desity':1.0
+         'density':1.0*/
+         isStatic: true
+
      }
      this.body = Bodies.rectangle(x,y,width,height,options);
      this.width = width;
      this.height = height;   
-     //this.image = loadImage("evilball.png");
+     this.image = loadImage("evilball.png");
      World.add(world, this.body);
     }
     display(){
-        var angle = this.body.angle;
         push();
         translate(this.body.position.x, this.body.position.y);
-        rotate(angle);
         rect(0,0,this.width,this.height);
-        //imageMode(CENTER);
-        //image(this.image,0,0,this.width, this.height);
+        image(this.image,0,0,this.width, this.height);
         pop();
     }
 }
--- sketch.js
+++ sketch.js
@@ -5,13 +5,25 @@ const Constraint = Matter.Constraint;
 
 var engine, world;
 var redball, evilbox;
+var movinglog1;
 
 function setup() {
   createCanvas(800,400);
-  evilbox = new box(200,100,60,60);
+  engine = Engine.create();
+  world = engine.world;
+
+
+  evilbox = new Box(200,100,60,60);
+  redball = new RedBall(300,100,60,60);
+  movinglog1 = new Log(400,100,100,20);
 }
 
 function draw() {
   background(255,255,255);  
+  Engine.update(engine);
+
+  evilbox.display();
+  redball.display();
+  movinglog1.display();
   drawSprites();
 }

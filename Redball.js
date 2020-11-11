91e293dd9f9fa8ed08b35874747c9b342b95b21b
Krishna Thakur <71776030+KrishnaThakur117@users.noreply.github.com>
2020-11-11 17:24:00 +0400

    Add files via upload

--- Redball.js
+++ Redball.js
@@ -1,10 +1,9 @@
 class RedBall{
     constructor(x,y,width,height){
         var options = {
-           /* 'restitution':0.8,
+           'restitution':0.8,
            'friction':1.0,
-           'density':1.0*/
-           isStatic: true
+           'density':1.0
         }
         this.body = Bodies.rectangle(x,y,width,height,options);
         this.width = width;
@@ -15,7 +14,7 @@ class RedBall{
     display(){
         push();
         translate(this.body.position.x, this.body.position.y);
-        rect(0,0,this.width,this.height);
+        imageMode(CENTER);
         image(this.image,0,0,this.width, this.height);
         pop();
     }
--- sketch.js
+++ sketch.js
@@ -6,26 +6,22 @@ const Constraint = Matter.Constraint;
 var engine, world;
 var redball, evilbox;
 var movinglog1;
-var movinglog2, movinglog3;
-var movinglog4,movinglog5;
+var movinglog2; 
 var ground1,ground2,ground3;
 
 function setup() {
-  createCanvas(1800,1400);
+  createCanvas(1250,550);
   engine = Engine.create();
   world = engine.world;
 
 
-  evilbox = new Box(200,100,60,60);
-  redball = new RedBall(300,100,60,60);
-  movinglog1 = new Log(400,100,100,20);
-  movinglog2 = new Log(550,100,100,20);
-  movinglog3 = new Log(700,100,100,20);
-  movinglog4 = new Log(850,100,100,20);
-  movinglog5 = new Log(1000,100,100,20);
-  ground1 = new Ground(400,500,100,80);
-  ground2 = new Ground(600,500,100,80);
-  ground3 = new Ground(800,500,100,80);
+  evilbox = new Box(650,200,60,60);
+  redball = new RedBall(40,200,60,60);
+  movinglog1 = new Log(255,200,100,20);
+  movinglog2 = new Log(880,200,100,20);
+  ground1 = new Ground(10,400,300,400);
+  ground2 = new Ground(625,400,300,400);
+  ground3 = new Ground(1240,400,300,400);
 }
 
 function draw() {
@@ -36,11 +32,14 @@ function draw() {
   redball.display();
   movinglog1.display();
   movinglog2.display();
-  movinglog3.display();
-  movinglog4.display();
-  movinglog5.display();
   ground1.display();
   ground2.display();
   ground3.display();
   drawSprites();
+}
+
+function keyPressed(){
+  if(keyCode === 32){
+    Matter.Body.setPosition(redball.body,{x:40,y:100})
+  }
 }
